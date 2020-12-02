e548397ad26ef7839a98bdeb741260554f6f29cd
jlgoh <gohxjl@gmail.com>
2020-12-03 00:52:21 +0800

    commit react build folder

---
+++ frontend/build/precache-manifest.210f34b25217f362a110f1f6ab7b1f06.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "318cc6541c6d9ce759e720f61733ecca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc3989a40325a057d062",
+    "url": "/static/css/2.d4e7eff5.chunk.css"
+  },
+  {
+    "revision": "d92b93df5c30f2750d98",
+    "url": "/static/css/main.e0d93ea6.chunk.css"
+  },
+  {
+    "revision": "dc3989a40325a057d062",
+    "url": "/static/js/2.545390e3.chunk.js"
+  },
+  {
+    "revision": "1f07fa3a8b9debd5886e327afa3b8d29",
+    "url": "/static/js/2.545390e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d92b93df5c30f2750d98",
+    "url": "/static/js/main.6dcdba45.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "bdd64c32c73b850add593f6d70a24580",
+    "url": "/static/media/landing.bdd64c32.svg"
+  },
+  {
+    "revision": "333824722e5188078b1d07bf4209b45f",
+    "url": "/static/media/template.33382472.jpg"
+  },
+  {
+    "revision": "6f56e4e308a3bb900d5443db67d85f42",
+    "url": "/static/media/template.6f56e4e3.mp3"
+  }
+]);

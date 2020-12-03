71eab5b3bed04f8d724d1df213e5ef8e6e2618e0
jlgoh <gohxjl@gmail.com>
2020-12-04 01:18:37 +0800

    Fix serving React static assets error

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -2,7 +2,6 @@
     This folder (/controllers) will contain all the endpoints for this flask application.
 """
 
-from controllers.home_controller import home_controller
 from controllers.user_controller import user_controller
 from controllers.project_controller import project_controller
 
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import secrets
 import os
-
-from flask import Flask
+import sys
+from flask import Flask, send_from_directory
 from flask_cors import CORS
 
 import models  # Import the models directory to create schema on MySQL DB.
@@ -15,7 +15,6 @@ URI = 'mysql+pymysql://{0}:{1}@{2}:{3}/{4}'.format(USER, PASSWORD, HOST, PORT, S
 # Function to create blueprints and initialise db
 def register_extensions(app_obj):
     app_obj.app_context().push()
-    app_obj.register_blueprint(home_controller, url_prefix="/")
     app_obj.register_blueprint(user_controller, url_prefix="/api/users")
     app_obj.register_blueprint(project_controller, url_prefix="/api/projects")
     db.init_app(app_obj)
@@ -24,7 +23,7 @@ def register_extensions(app_obj):
 
 # Function to configure database with Flask
 def create_app(name):
-    app_obj = Flask(name, static_folder='./frontend/build', static_url_path = "/")
+    app_obj = Flask(name, static_folder='frontend/build')
     app_obj.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DB_CONN")     #Link to connect db: 'mysql+pymysql://root:toor@localhost:3306/ase'
     app_obj.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app_obj.secret_key = secrets.token_urlsafe(32)
@@ -35,15 +34,16 @@ def create_app(name):
 
 app = create_app(__name__)
 
-# Production build files for React
-@app.route('/', methods=["GET"])
-def index():
-    return app.send_static_file('index.html')
+# Serve React Static Assets
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    print(path, file=sys.stdout)
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
 
-# Client Side Routing
-@app.errorhandler(404)
-def not_found(e):
-    return app.send_static_file('index.html')
 
 
 if __name__ == '__main__':
